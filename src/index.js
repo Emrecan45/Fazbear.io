826239eb1a6ae240100ddc5b5f21a9699aed0bf1
Emrecan45 <demirtay.e@gmail.com>
2026-03-29 13:43:26 +0200

    fix: respecter MVC + modifications visuelles

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ import BoutiqueView from "./views/BoutiqueView.js";
 import DetailCharacterView from "./views/DetailCharacterView.js";
 import CatalogueView from "./views/CatalogueView.js";
 
+let header = null;
+
 function cacherToutesLesSections() {
   let sections = document.querySelectorAll(".section-page");
   for (let i = 0; i < sections.length; i++) {
     sections[i].style.display = "none";
     sections[i].innerHTML = "";
   }
+  header = document.getElementById("header-home");
+  header.style.textDecoration = "none";
+  header = document.getElementById("header-catalogue");
+  header.style.textDecoration = "none";
+  header = document.getElementById("header-inventaire");
+  header.style.textDecoration = "none";
+  header = document.getElementById("header-boutique");
+  header.style.textDecoration = "none";
 }
 
 async function router() {
@@ -20,11 +30,15 @@ async function router() {
   document.body.classList.remove('boutique', 'accueil');
 
   if (request.resource === "accueil" || request.resource === "" || request.resource === "/") {
+    header = document.getElementById("header-home");
+    header.style.textDecoration = "underline";
     document.getElementById("accueil").style.display = "block";
     document.body.classList.add('accueil');
     await AccueilView.render();
   } 
   else if (request.resource === "catalogue") {
+    header = document.getElementById("header-catalogue");
+    header.style.textDecoration = "underline";
     // si demande un personnage via catalogue
     if (request.id === 'personnage' && request.verb) {
       document.getElementById("personnage").style.display = "block";
@@ -35,15 +49,19 @@ async function router() {
     }
   }
   else if (request.resource === "inventaire") {
+    header = document.getElementById("header-inventaire");
+    header.style.textDecoration = "underline";
     // si demande un personnage via inventaire
     if (request.id === 'personnage' && request.verb) {
       document.getElementById("personnage").style.display = "block";
       await DetailCharacterView.render(request.verb, 'inventaire');
     } else {
       document.getElementById("inventaire").style.display = "block";
-      await InventaireView.render();
+      await InventaireView.render(request.id || 'personnages');
     }
   } else if (request.resource === "boutique") {
+    header = document.getElementById("header-boutique");
+    header.style.textDecoration = "underline";
     document.getElementById("boutique").style.display = "block";
     document.body.classList.add('boutique');
     await BoutiqueView.render();
